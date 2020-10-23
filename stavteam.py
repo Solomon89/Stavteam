bc6f0b1ff1b0d0cceade31989162f0b7e05bcc35
Дмитрий Владимирович Соломонов <solomon89@list.ru>
2020-10-23 15:38:25 +0300

    View точка входа для python

--- stavteam.py
+++ stavteam.py
@@ -4,8 +4,4 @@ from flask import render_template
 app = Flask(__name__)
 app.debug = True
 
-
-
-@app.route('/info')
-def info():
-    return render_template("info.html", user = "Stavteam")
+from app import views
