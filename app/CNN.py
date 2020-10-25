c03876df029b5e4226f732a5c0f927ca8b2d6bb4
Дмитрий Владимирович Соломонов <solomon89@list.ru>
2020-10-25 09:58:17 +0300

    123

--- app/CNN.py
+++ app/CNN.py
@@ -9,16 +9,16 @@ from comtrade import Comtrade
     #SaveCNN(y_train)
     #return vector
 
-def TrainAPT2(Vector):
-    from numpy import exp, array, random, dot
-    training_set_inputs = Vector
-    training_set_outputs = Vector[:1].T
-    random.seed(1)
-    synaptic_weights = 2 * random.random((3, 1)) - 1
-    for iteration in xrange(10000):
-        output = 1 / (1 + exp(-(dot(training_set_inputs, synaptic_weights))))
-        synaptic_weights += dot(training_set_inputs.T, (training_set_outputs - output) * output * (1 - output))
-    print 1 / (1 + exp(-(dot(array([1, 0, 0]), synaptic_weights))))
+#def TrainAPT2(Vector):
+    #from numpy import exp, array, random, dot
+    #training_set_inputs = Vector
+    #training_set_outputs = Vector[:1].T
+    #random.seed(1)
+    #synaptic_weights = 2 * random.random((3, 1)) - 1
+    #for iteration in xrange(10000):
+    #    output = 1 / (1 + exp(-(dot(training_set_inputs, synaptic_weights))))
+   #     synaptic_weights += dot(training_set_inputs.T, (training_set_outputs - output) * output * (1 - output))
+   # print 1 / (1 + exp(-(dot(array([1, 0, 0]), synaptic_weights))))
 
 def SaveCNN(weights):
     #добавить сохраниение сети
