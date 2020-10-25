3a544db69c570662a547ef1f87ebffb5ffe6aa73
Tema <brykov93@gmail.com>
2020-10-25 08:35:11 +0300

    getRecorderInfo

--- app/views.py
+++ app/views.py
@@ -6,7 +6,6 @@ from app import dbFunctions
 from app import graph
 from random import randint
 
-
 deleteInterval = 3600
 
 
@@ -64,10 +63,22 @@ def getMap():
             recorderOutStation = stations[recorderOut['stationId']]
             coordInStation = {'lat': recorderInStation['lat'], 'lon': recorderInStation['lon']}
             coordOutStation = {'lat': recorderOutStation['lat'], 'lon': recorderOutStation['lon']}
-            lines[link] = {'coordInStation': coordInStation,
+            lines[link] = {'recorderIn': recorderIn,
+                           'recorderOut': recorderOut,
+                           'coordInStation': coordInStation,
                            'coordOutStation': coordOutStation}
         return jsonify({'lines': lines,
                         'stations': stations})
+    else:
+        abort(401)
+
+
+@app.route('/getRecorderInfo', methods=['POST'])
+def getRecorderInfo():
+    param = request.get_json()
+    if dbFunctions.checkSession(param['session']):
+        recorders = dbFunctions.getRecorders(True)
+        return jsonify(recorders[param['recoderId']])
     else:
         abort(401)
 
@@ -78,11 +89,11 @@ def graphs(event_id):
 
 
 @app.route('/getgraph/<int:event_id>/<int:id>/<path:typeGraph>')
-def getgraph(event_id =0, id = 0,typeGraph = "analog"):
+def getgraph(event_id=0, id=0, typeGraph="analog"):
     way = dbFunctions.getEventFilePath(event_id)
     way = getWayToRecorderIdStatus()
-    data = graph.GetGraphs(way,id,typeGraph)
-    return  data
+    data = graph.GetGraphs(way, id, typeGraph)
+    return data
 
 
 @app.route('/getgraph/<int:event_id>')
@@ -96,22 +107,21 @@ def getgraphInfo(event_id):
 def getWayToRecorderIdStatus():
     id = randint(0, 9)
 
-
-    if(id == 0):
+    if (id == 0):
         return "static//Oscillogramm//04JUL205"
-    elif(id== 1):
+    elif (id == 1):
         return "static//Oscillogramm//04APR163"
-    elif(id== 2):
+    elif (id == 2):
         return "static//Oscillogramm//5DES2019"
-    elif(id== 3):
+    elif (id == 3):
         return "static//Oscillogramm//5DES20192"
-    elif(id== 4):
+    elif (id == 4):
         return "static//Oscillogramm//16DES2019"
-    elif(id== 5):
+    elif (id == 5):
         return "static//Oscillogramm//17DES2019"
-    elif(id== 6):
+    elif (id == 6):
         return "static//Oscillogramm//21DES2019"
-    elif(id== 7):
+    elif (id == 7):
         return "static//Oscillogramm//27DES2019"
-    elif(id== 8):
+    elif (id == 8):
         return "static//Oscillogramm//29NOVEM2019"
